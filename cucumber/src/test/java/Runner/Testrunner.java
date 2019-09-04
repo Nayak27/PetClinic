accb220d3ab6a332944dbedf03a570d5d3a431f1
Dinesh Kumar Nayak <52646692+Nayak27@users.noreply.github.com>
2019-09-04 18:33:10 +0530

    Update Testrunner.java

--- cucumber/src/test/java/Runner/Testrunner.java
+++ cucumber/src/test/java/Runner/Testrunner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)  // libraries
 
-@Cucumber.Options(format = {"pretty", "html:target/Destination"}) 
+@Cucumber.Options(features = "src/test/resources",tags = {"@CreateAbstarct" }, plugin = { "pretty", "html:taget/cucumber-html-repoty","json:target/cucumber.json","junit:target/cucumber.xml","rerun:target/rerun.txt" }, glue = {"com.AbstarctSubmission"}) 
 
 
 
