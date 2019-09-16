95b0481080689ff1e881660a80074ab5f3459002
Dinesh Kumar Nayak <52646692+Nayak27@users.noreply.github.com>
2019-09-16 17:06:52 +0530

    Update Testrunner.java

--- cucumber/src/test/java/Runner/Testrunner.java
+++ cucumber/src/test/java/Runner/Testrunner.java
@@ -1,4 +1,4 @@
-//package Runner;
+package Runner;
 
 //import org.junit.runner.RunWith;
 //import cucumber.api.SnippetType;
@@ -10,19 +10,14 @@
 //@Cucumber.Options(features = "src/test/resources",snippets = SnippetType.CAMELCASE,tags = {"@CreateAbstarct" }, plugin = { "pretty", "html:taget/cucumber-html-repoty","json:target/cucumber.json","junit:target/cucumber.xml","rerun:target/rerun.txt" }, glue = {"com.AbstarctSubmission"}) 
 //@CucumberOptions(features= "Feature",glue={"StepDefination"})
 
-package com.techprimers.testing;
+
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/resources",
-        glue = "com.techprimers.testing",
-        format = {"json:target/cucumber.json"}
-    
-)
+@Cucumber.Options(features = "src/test/resources",snippets = SnippetType.CAMELCASE,tags = {"@CreateAbstarct" }, plugin = { "pretty", "html:taget/cucumber-html-repoty","json:target/cucumber.json","junit:target/cucumber.xml","rerun:target/rerun.txt" }, glue = {"com.AbstarctSubmission"}) 
 
 public class Testrunner {
 
