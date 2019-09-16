6e4cf0ba0b2442a597c9bafa00454bb58aa8b33a
Dinesh Kumar Nayak <52646692+Nayak27@users.noreply.github.com>
2019-09-16 15:53:48 +0530

    Update Testrunner.java

--- cucumber/src/test/java/Runner/Testrunner.java
+++ cucumber/src/test/java/Runner/Testrunner.java
@@ -1,15 +1,26 @@
 package Runner;
 
-import org.junit.runner.RunWith;
-import cucumber.api.SnippetType;
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+//import org.junit.runner.RunWith;
+//import cucumber.api.SnippetType;
+//import cucumber.api.CucumberOptions;
+//import cucumber.api.junit.Cucumber;
 
-@RunWith(Cucumber.class)  // libraries
+//@RunWith(Cucumber.class)  // libraries
 
-@Cucumber.Options(features = "src/test/resources",snippets = SnippetType.CAMELCASE,tags = {"@CreateAbstarct" }, plugin = { "pretty", "html:taget/cucumber-html-repoty","json:target/cucumber.json","junit:target/cucumber.xml","rerun:target/rerun.txt" }, glue = {"com.AbstarctSubmission"}) 
+//@Cucumber.Options(features = "src/test/resources",snippets = SnippetType.CAMELCASE,tags = {"@CreateAbstarct" }, plugin = { "pretty", "html:taget/cucumber-html-repoty","json:target/cucumber.json","junit:target/cucumber.xml","rerun:target/rerun.txt" }, glue = {"com.AbstarctSubmission"}) 
 //@CucumberOptions(features= "Feature",glue={"StepDefination"})
 
+package com.techprimers.testing;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources",
+        glue = "com.techprimers.testing",
+        format = {"json:target/cucumber.json"}
 
 public class Testrunner {
 
