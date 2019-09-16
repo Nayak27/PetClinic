db033a5ade124f4d669888b9b92752bb004fc188
Dinesh Kumar Nayak <52646692+Nayak27@users.noreply.github.com>
2019-09-16 13:15:02 +0530

    Update StepTest.java

--- cucumber/src/test/StepDefination/StepTest.java
+++ cucumber/src/test/StepDefination/StepTest.java
@@ -19,7 +19,7 @@ public class StepTest {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-        driver.get("http://ec2-52-15-240-27.us-east-2.compute.amazonaws.com:8080/petclinic");
+        driver.get("http://192.168.1.100:8090/petclinic");
 		
 	}
 
